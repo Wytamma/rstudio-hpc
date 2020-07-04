92ae51a761b938f6044000c430aedd14e446391d
Wytamma Wirth <wytamma.wirth@me.com>
2020-07-04 18:21:22 +1000

    :art: init scripts

---
+++ README.md
@@ -0,0 +1,71 @@
+# Integrating rstudio with the hpc
+
+This repo has some scripts to get RStudio running on the compute nodes of the HPC. This is achieved though a combination of singularity and ssh commands. 
+
+## Before you start 
+
+You should need to have some idea of how to use singularity.
+
+If you're not sure were to start work though this tutorial and associated webinars https://pawseysc.github.io/singularity-containers/
+
+## Instructions
+
+Clone this repo into your home directory on the HPC
+
+```bash
+https://github.com/Wytamma/rstudio-hpc.git && cd rstudio-hpc
+```
+
+Pull the RStudio container 
+
+```bash
+singularity pull library://wytamma/default/rstudio_hpc:latest
+```
+
+Submit the `start_rstudio.sh` script to `qsub`
+
+```bash
+qsub rstudio-hpc/start_rstudio.sh -o ~/rstudio-hpc/output/
+```
+
+Check the log file for instructions on how to connect to the rstudio server
+
+```bash
+cat log.txt
+```
+
+In a new terminal use the `ssh` command from the log file to 
+
+```bash
+ssh -L 8787:${HOSTNAME}:${PORT} ${USER}@zodiac.hpc.jcu.edu.au -p 8822 # -p for off-campus
+```
+
+Point your web browser to http://localhost:8787
+
+Log in to RStudio Server using the following credentials:
+
+```bash  
+user: ${USER}
+password: ${PASSWORD}
+```
+
+When done using RStudio Server, terminate the job by:
+
+1. Exit the RStudio Session ('power' button in the top right corner of the RStudio window)
+2. Issue the following command on the login node:
+
+```bash
+qdel -x ${PBS_JOBID}
+```
+
+## Explanation 
+...
+
+
+## Build the RStudio container yourself
+
+You could modify this to make changes to the container i.e. by changing the .def file
+
+```bash
+singularity build --remote rstudio-hpc.def rstudio-hpc.sif
+```
---
+++ start_rstudio.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#PBS -j oe
+#PBS -N r_studio 
+#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=4:mem=8gb
+#PBS -o ${HOME}/rstudio-hpc/output/
+
+# modified from https://www.rocker-project.org/use/singularity/
+
+export PASSWORD=$(openssl rand -base64 15)
+
+# get unused socket per https://unix.stackexchange.com/a/132524
+# tiny race condition between the python & singularity commands
+readonly PORT=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
+
+cat >> ${HOME}/rstudio-hpc/log.txt << END
+--- `date` ---
+1. SSH tunnel from your workstation using the following command:
+
+   ssh -L 8787:${HOSTNAME}:${PORT} ${USER}@zodiac.hpc.jcu.edu.au -p 8822 # -p for off-campus
+
+   and point your web browser to http://localhost:8787
+
+2. log in to RStudio Server using the following credentials:
+
+   user: ${USER}
+   password: ${PASSWORD}
+
+When done using RStudio Server, terminate the job by:
+
+1. Exit the RStudio Session ('power' button in the top right corner of the RStudio window)
+2. Issue the following command on the login node:
+
+      qdel -x ${PBS_JOBID}
+END
+
+# make ssh key for passwordless internode ssh
+# if you already have a public key copy it to .ssh/authorized_keys
+if [ ! -e ${HOME}/.ssh/id_rsa.pub ]
+  cat /dev/zero | ssh-keygen -t rsa
+  cat ${HOME}/.ssh/id_rsa.pub >> ${HOME}/.ssh/authorized_keys
+  chmod 700 ${HOME}/.ssh; chmod 640 ${HOME}/.ssh/authorized_keys
+fi
+
+# User-installed R packages go into their home directory
+if [ ! -e ${HOME}/.Renviron ]
+then
+  printf '\nNOTE: creating ~/.Renviron file\n\n'
+  echo 'R_LIBS_USER=~/R/%p-library/%v' >> ${HOME}/.Renviron
+  # env vars need to go in the .Renviron file
+fi
+
+# By default the only host file systems mounted within the container are $HOME, /tmp, /proc, /sys, and /dev.
+# you can use --bind [-B] to bind other file systems
+singularity exec ${HOME}/rstudio-hpc/rstudio_hpc_latest.sif bash -c "\
+  source ${HOME}/.Renviron && \
+  rserver --www-port ${PORT} --auth-none=0 --auth-pam-helper-path=pam-helper \
+  "
